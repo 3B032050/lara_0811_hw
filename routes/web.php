696b04c5571d6056dd928556a1f3e65a6accdfc1
黃文輝 <antory040512@gmail.com>
2023-10-13 16:37:20 +0800

    新增網頁跟index

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,13 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\HomeController;
+route::get('home',[HomeController::class,'index'])->name('home.index');
+use App\Http\Controllers\AboutController;
+route::get('home',[AboutController::class,'index'])->name('about.index');
+use App\Http\Controllers\NewController;
+route::get('home',[NewsController::class,'index'])->name('news.index');
+
 
 /*
 |--------------------------------------------------------------------------
