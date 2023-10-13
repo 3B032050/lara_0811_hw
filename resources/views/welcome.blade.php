5672631b9b7c88a9748468529d1ea6920a5fc1b8
黃文輝 <antory040512@gmail.com>
2023-10-13 16:44:27 +0800

    新增超連結到網頁

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -16,6 +16,9 @@
         </style>
     </head>
     <body class="antialiased">
+    <a href="{{route('about.index')}}">關於本站</a> 
+    <a href="{{route('news.index')}}">最新消息</a> 
+    <a href="{{route('home.index')}}">首頁</a> 
         <div class="relative sm:flex sm:justify-center sm:items-center min-h-screen bg-dots-darker bg-center bg-gray-100 dark:bg-dots-lighter dark:bg-gray-900 selection:bg-red-500 selection:text-white">
             @if (Route::has('login'))
                 <div class="sm:fixed sm:top-0 sm:right-0 p-6 text-right z-10">
